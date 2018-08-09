6edcdea30624f8f3eacf237deae50cd1060a7021
Paul Kinuthia <pkinuthia10@gmail.com>
2018-08-09 05:01:05 -0700

    [Feature #159633238] Question/answers/comment widgets (#5)

--- UI/js/main.js
+++ UI/js/main.js
@@ -3,12 +3,11 @@ const a = document.querySelectorAll("li a");
 for (let i = 0; i < lis.length; i++) {
     
   lis[i].addEventListener("click", function() {
-    console.log('sdfsff...');
     for (let i = 0; i < lis.length; i++) {
-      lis[i].classList.remove("active");
+      lis[i].classList.remove("sidebar-items__nav--active");
       a[i].classList.remove("active-text");
     }
-    this.classList.add("active");
+    this.classList.add("sidebar-items__nav--active");
     a[i].classList.add("active-text");
   });
 }
