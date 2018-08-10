5bc7193fe9a1134d0be9cc7f1f8a892ae460488a
p8ul <pkinuthia10@gmail.com>
2018-08-10 10:45:13 +0300

    [Feature #159632487] add vote icon event listener to ncrement votes

--- UI/js/main.js
+++ UI/js/main.js
@@ -11,3 +11,25 @@ for (let i = 0; i < lis.length; i++) {
     a[i].classList.add("active-text");
   });
 }
+
+
+const stats = document.querySelectorAll('.stats-box');
+const vote = document.querySelectorAll('.votes');
+const thumb = document.querySelectorAll('.vote_thumb');
+var count;
+
+// add events to selector
+for (let i = 0; i < stats.length; i++) {    
+  thumb[i].addEventListener("click", function() {
+    vote[i].classList.remove("fadeIn");
+
+    // Increment with some fadin animation
+    setTimeout(function(){
+      vote[i].classList.add("fadeIn");
+      count = vote[i].innerText;
+      vote[i].innerHTML = parseInt(count) + 1;
+      vote[i].classList.add("active-text");
+    }, 300); 
+        
+  });
+}
